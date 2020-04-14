4c3ea094627143bb31a7f6caf5b41617352575e6
TJF49240 <917352609@qq.com>
2020-04-14 20:27:19 +0800

    提交

--- zz-service/sys-service/src/main/java/com/zking/zzcloud/sysservice/model/Dict.java
+++ zz-service/sys-service/src/main/java/com/zking/zzcloud/sysservice/model/Dict.java
@@ -1,16 +1,16 @@
 package com.zking.zzcloud.sysservice.model;
 
-import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.extension.activerecord.Model;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author 凌
